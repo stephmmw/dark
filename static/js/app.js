c4bf78b3985286ab7e1ff2e2427317d349882a4e
stephmmw <stephenmmw@yahoo.com>
2022-11-04 18:03:15 +0300

    Update styles  and icons

--- static/js/app.js
+++ static/js/app.js
@@ -1,8 +1,25 @@
 jQuery(function($){
-    $("#school-logo").change(function(){
-        let $preview = document.getElementById("logo-preview");
+    // new logo
+    $("#new-logo").change(function(){
+        let $preview = document.getElementById("new-logo-preview");
         $("#"+$preview.id).empty();
-        let $image = document.getElementById("school-logo");
+        let $image = document.getElementById("new-logo");
+        let $imageFile = $image.files[0];       
+
+        if($imageFile){
+            const imageReader = new FileReader();            
+            imageReader.readAsDataURL($imageFile);
+            imageReader.addEventListener("load", function(){
+            $preview.innerHTML = '<img class="logo" src="' + this.result + '"/>';
+        });
+        }
+    });
+
+    // Update logo
+    $("#update-logo").change(function(){
+        let $preview = document.getElementById("update-logo-preview");
+        $("#"+$preview.id).empty();
+        let $image = document.getElementById("update-logo");
         let $imageFile = $image.files[0];       
 
         if($imageFile){
